e353699bbd4161aaa5f661b5e2142461c19ade86
hyunjusong <hun3780@gmail.com>
2020-08-29 20:39:35 +0900

    prettier

--- routes/links.js
+++ routes/links.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/links')
+const controller = require("../controllers/links");
 
 /* GET links listing. */
-router.get('/', controller.get);
-  //res.send('respond with a resource');
+router.get("/", controller.get);
+//res.send('respond with a resource');
 
-router.post('/', controller.post);
+router.post("/", controller.post);
 
-router.get('/:id' , controller.get_linkId);
+router.get("/:id", controller.get_linkId);
 
 module.exports = router;
