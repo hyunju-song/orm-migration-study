a35fb7891f576f93dc23731869a1ab5930b72289
hyunjusong <hun3780@gmail.com>
2020-08-30 14:24:23 +0900

    migration update 관련 추가 설명/수정필요

--- controllers/links/index.js
+++ controllers/links/index.js
@@ -11,14 +11,15 @@ module.exports = {
         //console.log(result.dataValues)
         return result.dataValues;
       });
-      res.send(data);
+      //res.send(data);
+      res.status(200).json(data);
     });
   },
 
   get_linkId: function (req, res) {
     //console.log(req)
     model.urls
-      .findByPk(req.params.id)
+      .findByPk(req.params.id) //findOne을 써도된다. 레퍼런스 코드 참고
       .then((result) => {
         //console.log(result.dataValues.visits+1)
         ///console.log(req.params.id)
@@ -44,18 +45,114 @@ module.exports = {
   post: function (req, res) {
     //console.log(req)
     let url = req.body.url;
+    if (!isValidUrl(url)) {
+      return res.sendStatus(400);
+    }
     let maketable = function (err, title) {
+      //같은 데이터가 있을 확률도 있으믈 findorcreate 메소드가 더 적절한듯 하다.
       model.urls
-        .create({
-          url,
-          title,
-          visits: 0,
+        .findOrCreate({
+          where: {
+            url: url,
+          },
+          defaults: {
+            title: title,
+          },
         })
-        .then((result) => {
-          //console.log(result)
-          res.status(201).send(result.dataValues);
+        .then(([result, created]) => {
+          if (!created) {
+            return res.status(201).json(result); // find
+          }
+          res.status(201).json(result); // Created
+        })
+        .catch((error) => {
+          console.log(error);
+          res.sendStatus(500); // Server error
         });
+      // model.urls
+      //   .create({
+      //     url,
+      //     title,
+      //     visits: 0,
+      //   })
+      //   .then((result) => {
+      //     //console.log(result)
+      //     res.status(201).json(result.dataValues);
+      //   })
+      //   .catch((err) => {
+      //     res.sendStatus(500);
+      //   });
     };
     getUrlTitle(url, maketable);
   },
 };
+
+/* ㄹㅔ퍼런스코드
+const utils = require('../../modules/utils');
+const { url: URLModel } = require('../../models');
+
+module.exports = {
+  get: async (req, res) => {
+    const result = await URLModel.findAll();
+    res.status(200).json(result);
+  },
+  post: (req, res) => {
+    const { url } = req.body;
+
+    if (!utils.isValidUrl(url)) {
+      return res.sendStatus(400);
+    }
+
+    utils.getUrlTitle(url, (err, title) => {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(400);
+      }
+
+      URLModel
+        .findOrCreate({
+          where: {
+            url: url
+          },
+          defaults: {
+            title: title
+          }
+        })
+        .then(([result, created]) => {
+          if (!created) {
+            return res.status(201).json(result); // find
+          }
+          res.status(201).json(result); // Created
+        })
+        .catch(error => {
+          console.log(error);
+          res.sendStatus(500); // Server error
+        });
+    });
+  },
+  redirect: (req, res) => {
+    URLModel
+      .findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(result => {
+        if (result) {
+          return result.update({
+            visits: result.visits + 1
+          });
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .then(result => {
+        res.redirect(result.url);
+      })
+      .catch(error => {
+        console.log(error);
+        res.sendStatus(500);
+      });
+  }
+}
+*/
---
+++ seeders/20200830051516-demo-urls.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("urls", [
+      {
+        url: "www.naver.com",
+        title: "네이버",
+        visits: 0,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("urls", null, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
